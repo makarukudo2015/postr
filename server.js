2061188f240e344f15f10ed9269d672ff0686025
makarukudo <bruczki21@live.com>
2015-11-11 08:21:21 +0000

    Adding gulp and other dependencies

---
+++ server.js
@@ -0,0 +1,18 @@
+var restify = require('restify');
+
+var server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+});
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.get('/echo/:name', function (req, res, next) {
+    res.send("Test" + req.params.name);
+    return next();
+});
+
+server.listen(process.env.PORT || 3001, process.env.IP || "0.0.0.0", function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
